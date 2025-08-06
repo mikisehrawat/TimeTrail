e3096851f533825ce394c442ee2686dfb6438fee
Miki Sehrawat <sahiljaatbirohar@gmail.com>
2025-08-06 14:25:32 +0530

    setting up spring boot

---
+++ APIS/src/main/java/com/TimeTrail/TimeTrail/TimeTrailApplication.java
@@ -0,0 +1,13 @@
+package com.TimeTrail.TimeTrail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeTrailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeTrailApplication.class, args);
+	}
+
+}
---
+++ APIS/src/test/java/com/TimeTrail/TimeTrail/TimeTrailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TimeTrail.TimeTrail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeTrailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
